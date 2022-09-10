87abe22cbe2eca3d3dce3baa1d98985905b28957
Alejandro Bay <alejandrobay23@gmail.com>
2022-09-10 16:18:58 -0400

    Side Bar Update

--- src/Header.js
+++ src/Header.js
@@ -3,6 +3,9 @@ import './Header.css';
 import SearchIcon from '@mui/icons-material/Search';
 import HomeIcon from '@mui/icons-material/Home';
 import SupervisorAccountIcon from '@mui/icons-material/SupervisorAccount';
+import BusinessCenterIcon from '@mui/icons-material/BusinessCenter';
+import ChatIcon from '@mui/icons-material/Chat';
+import NotificationsIcon from '@mui/icons-material/Notifications';
 import HeaderOption from './HeaderOption';
 
 function Header() {
@@ -22,6 +25,13 @@ function Header() {
         {/* HEader reusable component */}
         <HeaderOption Icon={HomeIcon} title='Home' />
         <HeaderOption Icon={SupervisorAccountIcon} title='My Network' />
+        <HeaderOption Icon={BusinessCenterIcon} title='Jobs' />
+        <HeaderOption Icon={ChatIcon} title='Messaging' />
+        <HeaderOption Icon={NotificationsIcon} title='Notifications' />
+        <HeaderOption
+          avatar='https://avatars.githubusercontent.com/u/45724167?v=4'
+          title='me'
+        />
       </div>
     </div>
   );
